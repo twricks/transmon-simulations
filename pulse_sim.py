bb51d0f4fe1866a7eaad8696ece1e9b875aabc2c
Tom R <thomaswricksjr@gmail.com>
2026-05-09 18:24:29 -0400

    Update pulse_sim.py

--- pulse_sim.py
+++ pulse_sim.py
@@ -2,43 +2,36 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from guppylang import guppy
-from guppylang.std.builtins import result
+from guppylang.std.builtins import result, comptime
 from guppylang.std.quantum import qubit, measure, rx
-
-
-@guppy
-def experiment(theta) -> None:
-    q = qubit()
-    rx(q, theta)
-    outcome = measure(q)
-    result("Excited", outcome)
-
+from guppylang.std.angles import angle
 
 thetas = np.linspace(0, 2*np.pi, 100)
 shots_per_point = 20
 
 populations = []
 
-emulator = experiment.emulator(n_qubits=1)
-
 for theta in thetas:
+    @guppy
+    def experiment() -> None:
+        q = qubit()
+        q = rx(q,angle((comptime(theta))))
+        outcome = measure(q)
+        result("Excited",outcome)
+    
+    experiment.check()
 
-    shots = (
-        emulator
-        .with_shots(shots_per_point)
-        .run(theta=theta)
-    )
+    shots = (experiment.emulator(n_qubits=1).with_shots(shots_per_point).run())
 
     excited_count = sum(
         1 for shot in shots
-        if shot.as_dict().get("Excited", False)
+        if shot.as_dict().get("excited",False)
     )
 
     pop = excited_count / shots_per_point
     populations.append(pop)
 
-    print(f"angle = {theta:.3f} -> population = {pop:.3f}")
-
+    #print()
 
 plt.figure(figsize=(8,5))
 plt.plot(thetas, populations, 'o-')
